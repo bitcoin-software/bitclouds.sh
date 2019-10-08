3241f9cad2c89b0327bcd589fcdb6dbca8c9e4f8
bebebe-cmd <b@ysx.in>
2019-10-08 19:24:59 +0300

    add electrum

---
+++ wallet/listener.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse, request
+import datetime
+from btc_wallet import bstartd, bgetunused, bgetnew, bnotify
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+app = Flask(__name__)
+ipn = Api(app)
+
+
+class elify(Resource):
+    def post(self):
+        #parser = reqparse.RequestParser()
+        #parser.add_argument('token')
+        #args = parser.parse_args()
+        #dtime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+        #try:
+        #    token = args['token']
+        #except KeyError as e:
+        #    print(dtime + ' no data' + str(e))
+        #    return False
+
+        print(request.get_data())
+
+
+if __name__ == '__main__':
+
+    app.run(debug=False, port=16333)
+
