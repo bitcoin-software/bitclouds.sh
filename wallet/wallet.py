b0e17e4386f73505f2cd63f82f4d75187e3eaf12
bebebe-cmd <b@ysx.in>
2019-10-08 16:15:39 +0300

    add electrum

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -1,17 +1,39 @@
-from bitcart.coins.btc import BTC
+from flask import Flask
+from flask_restful import Resource, Api, reqparse, request
+import datetime
+from btc_wallet import bstartd, bgetunused, bgetnew, bnotify
 import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
-btc = BTC(xpub=str(config['electrum']['xpub']))
-
-
-@btc.on("new_transaction")
-def callback_func(event, tx):
-    print(event)
-    print(tx)
+app = Flask(__name__)
+ipn = Api(app)
 
 
-btc.poll_updates()
+class elify(Resource):
+    def post(self):
+        #parser = reqparse.RequestParser()
+        #parser.add_argument('token')
+        #args = parser.parse_args()
+        #dtime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+        #try:
+        #    token = args['token']
+        #except KeyError as e:
+        #    print(dtime + ' no data' + str(e))
+        #    return False
+
+        print(request.data)
+
+
+if __name__ == '__main__':
+    bstartd()
+
+    ipn.add_resource(elify, '/elify')
+
+    notifyURL = config['ipn']['url'] + '/elify'
+    print(bgetunused())
+    print(bnotify('addr', notifyURL))
+
+    app.run(debug=False, port=16333)
+
