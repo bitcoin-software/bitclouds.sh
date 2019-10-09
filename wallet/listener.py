d7dbd2b9f570a099dc606d1031799f2f889c5d87
bebebe-cmd <b@ysx.in>
2019-10-09 13:31:59 +0300

    money accounting

--- wallet/listener.py
+++ wallet/listener.py
@@ -1,4 +1,10 @@
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+dbclient = pymongo.MongoClient('localhost')
+mongo_db = "hosting"
+mongo = dbclient[mongo_db]
 
-access = AuthServiceProxy("http://ae059511:8s5r2X_EwFBrg1l6GMQvFfn2cqXFE73ud7MpPVZUx7I=@rpc.bitbsd.org:8332")
-print(access.gettransaction('cc53e04e47ff4cd52cf1855a0dac7ac764c3ae3d23f3059e034515895967f3db'))
+hosts = mongo.hosts.find()
+
+txs = mongo.txs.find()
+
+print(hosts)
+print(txs)
