f0adf7fe7d676bba95190a12e4f30ef5c4f603c9
bebebe-cmd <b@ysx.in>
2019-10-09 10:49:59 +0300

    first launch

--- wallet/btc_wallet.py
+++ wallet/btc_wallet.py
@@ -1,10 +1,7 @@
 import subprocess
 import time
 import json
-from jsonrpc import ServiceProxy
 
-access = ServiceProxy("http://ae059511:8s5r2X_EwFBrg1l6GMQvFfn2cqXFE73ud7MpPVZUx7I=@rpc.bitbsd.org:8332")
-access.getinfo()
 
 btcbinpath = '/usr/local/bin/electrum'
 
