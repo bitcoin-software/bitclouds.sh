0a9eecb969fc0dac4646e9c9aeed528b3119fbe3
bebebe-cmd <b@ysx.in>
2019-10-09 17:36:43 +0300

    new servers

--- wallet/listener.py
+++ wallet/listener.py
@@ -10,6 +10,10 @@ hosts = mongo.hosts.find()
 
 txs = mongo.txs.find()
 
+
+hetzners = mongo.hetzner.find()
+
+
 arg = sys.argv[1]
 
 if arg == 'list':
@@ -18,6 +22,10 @@ if arg == 'list':
 
     for tx in txs:
         print(tx)
+
+    for server in hetzners:
+        print(server)
+
 elif arg == 'del':
     dbclient.drop_database(mongo_db)
 
