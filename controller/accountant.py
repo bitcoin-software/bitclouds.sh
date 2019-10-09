910e56ae3462a51681dfac44af397e89b3bee982
bebebe-cmd <b@ysx.in>
2019-10-09 16:16:31 +0300

    money accounting

--- controller/accountant.py
+++ controller/accountant.py
@@ -5,9 +5,16 @@ config = configparser.ConfigParser()
 config.read('../controller/config.ini')
 
 from dbops import find_hosts, deduct_host
+from hetzner import createServer, deleteServer
 
 hosts = find_hosts()
 for host in hosts:
     if host['status'] == 'subscribed':
         deduct_host(host['address'])
         print(host['address'] + "is subscribed; balance: " + str(host['balance']))
+
+serverdata = createServer('NAMENAME')
+
+print("created: " + str(serverdata))
+
+deleteServer(serverdata['id'])
--- controller/hetzner.py
+++ controller/hetzner.py
@@ -1,3 +1,61 @@
 
-hetzner_api = "4JTtV7d13fLuHFs0HK1avYaiQR4wV1NJYAXD8hqSbfWTOoZcLrNkcx7TJcEDbTVN"
+from hcloud import Client
 
+from hcloud.images.domain import Image
+from hcloud.server_types.domain import ServerType
+from hcloud.ssh_keys.client import SSHKeysClient
+
+
+import configparser
+
+hetzner_config = configparser.ConfigParser()
+hetzner_config.read('/home/bitclouds/bitclouds/controller/config.ini')
+
+hetzner_token = hetzner_config['hetzner']['api_key']
+
+client = Client(token=hetzner_token)  # Please paste your API token here between the quotes
+sshClient = SSHKeysClient(client)
+
+
+def getServers():
+    hetzner_servers = client.servers.get_all()
+
+    servers = list()
+
+    for server in hetzner_servers:
+        serverData = dict()
+
+        serverData['id'] = server.id
+        serverData['ip'] = server.public_net.ipv4.ip
+        serverData['name'] = server.name
+
+        servers.append(serverData)
+
+    return servers
+
+
+def createServer(name, snapid="8322744"):
+    mykeys = list()
+    for key in sshClient.get_all():
+        mykeys.append(key)
+
+    response = client.servers.create(name=name, server_type=ServerType("cx11"), image=Image(type="snapshot", id=snapid), ssh_keys=mykeys)
+    server = response.server
+
+    serverData = dict()
+
+    serverData['id'] = server.id
+    serverData['ip'] = server.public_net.ipv4.ip
+
+    return serverData
+
+
+def deleteServer(id):
+    mykeys = list()
+    for key in sshClient.get_all():
+        mykeys.append(key)
+
+    response = client.servers.delete(id=id)
+    server = response.server
+
+    return server
