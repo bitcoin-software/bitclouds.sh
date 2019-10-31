0c663cf74474829185f99951c9e69a514273a87e
bebebe-cmd <b@ysx.in>
2019-10-31 19:56:52 +0200

    add info

--- controller/accountant.py
+++ controller/accountant.py
@@ -13,6 +13,7 @@ from orchestrator import del_server
 hosts = find_hosts()
 for host in hosts:
     if host['status'] == 'subscribed':
+        print(host)
         deduct_host(host['address'])
         dtime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
         os.system("echo '" + dtime + ": " + host['address'] + "is subscribed; balance: " + str(host['balance']) + "' >> /tmp/acc.log")
