bd6eb4b4ce1a7acb0a33e31dae9d2ec9ecbafc05
bebebe-cmd <b@ysx.in>
2019-11-01 18:41:49 +0200

    add cred get

--- instance/host/get_rpc_cred.py
+++ instance/host/get_rpc_cred.py
@@ -5,7 +5,7 @@ with open("/zroot/jails/jails-data/bitcoin_rpc-data/usr/local/etc/bitcoin.conf",
     lines = []
     for ln in fi:
         if ln.startswith("#guest_rpc"):
-            lines.append(ln[2:])
+            lines.append(ln)
 
 line = random.choice(lines)
 
