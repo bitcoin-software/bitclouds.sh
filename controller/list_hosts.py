e3cdba006b526c19be00f849143dc85af7bd046c
bebebe-cmd <b@ysx.in>
2019-11-02 17:55:26 +0200

    add host deletion

--- controller/list_hosts.py
+++ controller/list_hosts.py
@@ -11,19 +11,18 @@ config.read('../controller/config.ini')
 from ctrldbops import find_hosts, deduct_host, get_suspended, delete_host, get_bitbsd
 from orchestrator import del_server
 
+hosts = find_hosts()
+
 if sys.argv[1] in ['new', 'subscribed', 'deleted', 'suspended']:
-    hosts = find_hosts()
     for host in hosts:
         if host['status'] == sys.argv[1]:
             print(host)
 elif sys.argv[1] == 'housekeeper':
-    hosts = find_hosts()
     for host in hosts:
         if host['status'] == 'deleted':
             del_server(host['address'])
 elif sys.argv[1] == 'del':
     host2del = sys.argv[2]
-    hosts = find_hosts()
     for host in hosts:
         if host['address'] == host2del:
             del_server(host['address'])
