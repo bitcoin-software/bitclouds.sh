243435e10bc53995910f9d350d68ddae2e098a9c
bebebe-cmd <b@ysx.in>
2019-11-05 16:14:32 +0200

    add support

--- wallet/tgcontrol.py
+++ wallet/tgcontrol.py
@@ -10,10 +10,9 @@ tg_config.read('../wallet/config.ini')
 
 tg_token = tg_config['telegram']['token']
 
-bot = telegram.Bot(tg_token)
-
 chat_id = tg_config['telegram']['admin_chatid']
 
+bot = telegram.Bot(tg_token)
 
 def ticket_notify(premium, address, contact, msg):
     if premium == '*':
@@ -29,6 +28,7 @@ def ticket_notify(premium, address, contact, msg):
             reminded += 1
         else:
             reminded = 100
+
         bot.send_message(chat_id=chat_id, text=header + 'Support request for <b>' + address +
                                               '</b>\n' + contact + '\n--- START ---\n<i>' + msg +
                                               '</i>\n--- END ---'
