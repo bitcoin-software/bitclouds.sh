360222b02e3875144c5a282f2844462e64729efe
bebebe-cmd <b@ysx.in>
2019-11-05 03:41:55 +0200

    add vpn

--- controller/hetzner.py
+++ controller/hetzner.py
@@ -48,7 +48,7 @@ def createServer(name, image):
         snap_id = "8322744"
         sysImage = False
     elif image == "vpn":
-        snap_id = "9206457"
+        snap_id = "9207785"
         sysImage = False
     elif image == "ubuntu":
         image_name = "ubuntu-18.04"
