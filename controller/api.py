81207339d26253a68076ea2d0a8e6e55ab9a6e6b
bebebe-cmd <b@ysx.in>
2019-11-05 16:24:51 +0200

    tg support

--- controller/api.py
+++ controller/api.py
@@ -91,8 +91,8 @@ def chkinv(inv):
     return False
 
 
-@app.route('/support/<address>/<string:contact>', defaults={'msg': 'empty', 'premium': 'regular'})
 @app.route('/support/<address>/<string:contact>/<string:msg>', defaults={'premium': 'regular'})
+@app.route('/support/<address>/<string:contact>', defaults={'msg': 'empty', 'premium': 'regular'})
 def support(address, contact, msg, premium):
     if len(msg) > 300:
         formatted_msg = msg[:300]
