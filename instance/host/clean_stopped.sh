f08a6d539c43f69fcdb11ae25e2860f89ee36003
bebebe-cmd <b@ysx.in>
2019-11-05 12:30:17 +0200

    add vpn

--- instance/host/clean_stopped.sh
+++ instance/host/clean_stopped.sh
@@ -1,4 +1,6 @@
 echo " Starting removal: " >> /var/log/jailremove.log
+dt=`date '+%d/%m/%Y_%H:%M:%S'`
+echo "$dt" >> /var/log/jailremove.log
 cbsd jls | egrep ' Off' >> /var/log/jailremove.log
 cbsd jls | egrep ' Off' | egrep -o 'cln[A-Za-z0-9]+  0' | sed 's/0$//' | xargs -n1 cbsd jstop >> /var/log/jailremove.log
 
