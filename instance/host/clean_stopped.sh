605a16ae91b5f3ea939fbf623c3378e72f74adfc
bebebe-cmd <b@ysx.in>
2019-11-06 03:44:29 +0200

    autoclean script

--- instance/host/clean_stopped.sh
+++ instance/host/clean_stopped.sh
@@ -5,7 +5,9 @@ cbsd jls | egrep ' Off' >> /var/log/jailremove.log
 echo "$dt removing clightning jails" >> /var/log/jailremove.log
 cbsd jls | egrep ' Off' | egrep -o 'cln[A-Za-z0-9][A-Za-z0-9]+  0' | sed 's/0$//' | xargs -n1 cbsd jremove >> /var/log/jailremove.log
 echo "$dt removing rootshell jails" >> /var/log/jailremove.log
-cbsd jls | egrep ' Off' | egrep -o 'rs[A-Za-z0-9][A-Za-z0-9][A-Za-z0-9]+  0' | sed 's/0$//' | xargs -n1 cbsd jremove >> /var/log/jailremove.log
+cbsd jls | egrep ' Off' | egrep -o 'rs[A-Za-z0-9][A-Za-z0-9][A-Za-z0-9]+   0' | sed 's/0$//' | xargs -n1 cbsd jremove >> /var/log/jailremove.log
+echo "$dt removing bitcoind jails" >> /var/log/jailremove.log
+cbsd jls | egrep ' Off' | egrep -o 'bd[A-Za-z0-9][A-Za-z0-9][A-Za-z0-9]+   0' | sed 's/0$//' | xargs -n1 cbsd jremove >> /var/log/jailremove.log
 cbsd jls
 
 
