e5073e673f0b2e5f0aa3047d36fe6ad535cd5e47
bebebe-cmd <b@ysx.in>
2019-11-06 02:53:30 +0200

    autoclean script

--- instance/host/clean_stopped.sh
+++ instance/host/clean_stopped.sh
@@ -2,5 +2,10 @@ echo " Starting removal: " >> /var/log/jailremove.log
 dt=`date '+%d/%m/%Y_%H:%M:%S'`
 echo "$dt" >> /var/log/jailremove.log
 cbsd jls | egrep ' Off' >> /var/log/jailremove.log
+echo "$dt removing clightning jails" >> /var/log/jailremove.log
 cbsd jls | egrep ' Off' | egrep -o 'cln[A-Za-z0-9]+  0' | sed 's/0$//' | xargs -n1 cbsd jstop >> /var/log/jailremove.log
+echo "$dt removing rootshell jails" >> /var/log/jailremove.log
+cbsd jls | egrep ' Off' | egrep -o 'rs[A-Za-z0-9]+  0' | sed 's/0$//' | xargs -n1 cbsd jstop >> /var/log/jailremove.log
+cbsd jls
+
 
