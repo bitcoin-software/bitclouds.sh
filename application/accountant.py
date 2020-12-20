19a20e1abff8a59ec749ba82d442e7c76c573710
bebebe-cmd <b@ysx.in>
2020-12-20 22:50:57 +0200

    modify notifier

--- application/accountant.py
+++ application/accountant.py
@@ -23,16 +23,38 @@ MARKET = ['k8s']
 
 
 def notify(bot_message):
-    bot_token = os.environ['TG_TOKEN']
-    bot_chatID = os.environ['TG_CHAT']
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + \
-               bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-
     try:
-        response = requests.get(send_text, timeout=3)
-        return response.json()
+        headers = {
+            'Content-Type': 'application/json',
+        }
+
+        data = '{"text": "'+ bot_message + '"}'
+
+        response = requests.post(os.environ['MSG_ENDPOINT'] + '/msg',
+                                 headers=headers, data=data, verify=False, timeout=9)
+
+        if response.status_code == 200:
+            sent = True
+        else:
+            sent = False
     except Exception as e:
-        return {'error': str(e)}
+        print('matrix sent error')
+        print(e)
+        tg_bot_token = os.environ['TG_TOKEN']
+        tg_bot_chatID = os.environ['TG_CHAT']
+        send_text = 'https://api.telegram.org/bot' + tg_bot_token + '/sendMessage?chat_id=' + \
+                   tg_bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+        try:
+            response = requests.get(send_text, timeout=3)
+            if response.status_code == 200:
+                sent = True
+            else:
+                sent = False
+        except Exception as e_tg:
+            print('telegram send error')
+            print(e_tg)
+            sent = False
+    return sent
 
 
 def get_random_string(length):
