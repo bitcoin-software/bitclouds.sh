755e8ca868a3609d6e86a40779cb5e3b75e147ef
bebebe-cmd <b@ysx.in>
2020-12-21 07:56:19 +0200

    add tickets

--- webpage/ticket.py
+++ webpage/ticket.py
@@ -15,6 +15,48 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.url_map.strict_slashes = False
 
 
+def notify(bot_message):
+
+    def tgsend():
+        tg_bot_token = os.environ['TG_TOKEN']
+        tg_bot_chatID = os.environ['TG_CHAT']
+        send_text = 'https://api.telegram.org/bot' + tg_bot_token + '/sendMessage?chat_id=' + \
+                    tg_bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+        try:
+            response = requests.get(send_text, timeout=3)
+            if response.status_code == 200:
+                msgsent = True
+            else:
+                msgsent = False
+        except Exception as e_tg:
+            print('telegram send error')
+            print(e_tg)
+            msgsent = False
+
+        return msgsent
+
+    try:
+        headers = {
+            'Content-Type': 'application/json',
+        }
+
+        data = '{"text": "' + bot_message + '"}'
+
+        response = requests.post(os.environ['MSG_ENDPOINT'] + '/bcmon',
+                                 headers=headers, data=data, verify=False, timeout=9)
+
+        if response.status_code == 200:
+            sent = True
+        else:
+            sent = tgsend()
+    except Exception as e:
+        print('matrix sent error')
+        print(e)
+        sent = tgsend()
+
+    return sent
+
+
 def generate_invoice(amount_sats, desc):
     dtime = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d-%H%M%S')
 
@@ -32,9 +74,13 @@ def generate_invoice(amount_sats, desc):
 
 @app.route('/ticket')
 def handle_data():
+    invoice = generate_invoice(99, str(request.values.get('instance'))+"-support")['bolt11']
     params = {
-        'field1': request.values.get('textfield'),
-        'field2': request.values.get('textfield2')
+        'instance': request.values.get('instance'),
+        'email': request.values.get('email'),
+        'msg': request.values.get('msg'),
+        'invoice': invoice
+
     }
 
     return jsonify(params)
